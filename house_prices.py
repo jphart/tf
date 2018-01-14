8233a0ad6398793b1788f05579c11703c07f179a
Jonathan Hart <jonathan@Minardi.home>
2018-01-14 19:20:29 +0000

    Update

--- house_prices.py
+++ house_prices.py
@@ -95,52 +95,50 @@ with tf.Session as sess:
     # Plot of training and test data, and learned regression
 
      # get values used to normalized data so we can denormalize data back to its original scale
-     train_house_size_mean = train_house_size.mean()
-     train_house_size_std = train_house_size.std()
+    train_house_size_mean = train_house_size.mean()
+    train_house_size_std = train_house_size.std()
 
-     train_price_mean = train_price.mean()
-     train_price_std = train_price.std()
+    train_price_mean = train_price.mean()
+    train_price_std = train_price.std()
 
-     # Plot the graph
-     plt.rcParams["figure.figsize"] = (10,8)
-     plt.figure()
-     plt.ylabel("Price")
-     plt.xlabel("Size (sq.ft)")
-     plt.plot(train_house_size, train_price, 'go', label='Training data')
-     plt.plot(test_house_size, test_house_price, 'mo', label='Testing data')
-     plt.plot(train_house_size_norm * train_house_size_std + train_house_size_mean,
-              (sess.run(tf_size_factor) * train_house_size_norm + sess.run(tf_price_offset)) * train_price_std + train_price_mean,
-              label='Learned Regression')
-
-     plt.legend(loc='upper left')
-     plt.show()
+    # Plot the graph
+    plt.rcParams["figure.figsize"] = (10,8)
+    plt.figure()
+    plt.ylabel("Price")
+    plt.xlabel("Size (sq.ft)")
+    plt.plot(train_house_size, train_price, 'go', label='Training data')
+    plt.plot(test_house_size, test_house_price, 'mo', label='Testing data')
+    plt.plot(train_house_size_norm * train_house_size_std + train_house_size_mean,
+          (sess.run(tf_size_factor) * train_house_size_norm + sess.run(tf_price_offset)) * train_price_std + train_price_mean,
+          label='Learned Regression')
+    plt.legend(loc='upper left')
+    plt.show()
 
 
 
      #
      # Plot another graph that animation of how Gradient Descent sequentually adjusted size_factor and price_offset to
      # find the values that returned the "best" fit line.
-     fig, ax = plt.subplots()
-     line, = ax.plot(house_size, house_price)
+    fig, ax = plt.subplots()
+    line, = ax.plot(house_size, house_price)
 
-     plt.rcParams["figure.figsize"] = (10,8)
-     plt.title("Gradient Descent Fitting Regression Line")
-     plt.ylabel("Price")
-     plt.xlabel("Size (sq.ft)")
-     plt.plot(train_house_size, train_price, 'go', label='Training data')
-     plt.plot(test_house_size, test_house_price, 'mo', label='Testing data')
+    plt.rcParams["figure.figsize"] = (10,8)
+    plt.title("Gradient Descent Fitting Regression Line")
+    plt.ylabel("Price")
+    plt.xlabel("Size (sq.ft)")
+    plt.plot(train_house_size, train_price, 'go', label='Training data')
+    plt.plot(test_house_size, test_house_price, 'mo', label='Testing data')
 
-     def animate(i):
-         line.set_xdata(train_house_size_norm * train_house_size_std + train_house_size_mean)  # update the data
-         line.set_ydata((fit_size_factor[i] * train_house_size_norm + fit_price_offsets[i]) * train_price_std + train_price_mean)  # update the data
-         return line,
+    def animate(i):
+        line.set_xdata(train_house_size_norm * train_house_size_std + train_house_size_mean)  # update the data
+        line.set_ydata((fit_size_factor[i] * train_house_size_norm + fit_price_offsets[i]) * train_price_std + train_price_mean)  # update the data
+        return line,
 
-      # Init only required for blitting to give a clean slate.
-     def initAnim():
-         line.set_ydata(np.zeros(shape=house_price.shape[0])) # set y's to 0
-         return line,
+    # Init only required for blitting to give a clean slate.
+    def initAnim():
+        line.set_ydata(np.zeros(shape=house_price.shape[0])) # set y's to 0
+        return line,
 
-     ani = animation.FuncAnimation(fig, animate, frames=np.arange(0, fit_plot_idx), init_func=initAnim,
-                                  interval=1000, blit=True)
-
-     plt.show()   
+    ani = animation.FuncAnimation(fig, animate, frames=np.arange(0, fit_plot_idx), init_func=initAnim,
+                              interval=1000, blit=True)
+    plt.show()   
