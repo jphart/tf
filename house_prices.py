e2f35d0933953565ee63250201e7a612f934afe1
Jonathan Hart <jonathan@Minardi.home>
2018-01-14 19:30:37 +0000

    Update

--- house_prices.py
+++ house_prices.py
@@ -66,15 +66,26 @@ optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(tf_cost)
 
 #Train the model!
 init = tf.global_variables_initializer()
-with tf.Session as sess:
+# Launch the graph in the session
+with tf.Session() as sess:
     sess.run(init)
 
+    # set how often to display training progress and number of training iterations
     display_every = 2
     num_training_iter = 50
 
+    # calculate the number of lines to animation
+    fit_num_plots = math.floor(num_training_iter/display_every)
+    # add storage of factor and offset values from each epoch
+    fit_size_factor = np.zeros(fit_num_plots)
+    fit_price_offsets = np.zeros(fit_num_plots)
+    fit_plot_idx = 0
+
+   # keep iterating the training data
     for iteration in range(num_training_iter):
 
-        for(x,y) in zip(train_house_size_norm,train_house_price_norm):
+        # Fit all training data
+        for (x, y) in zip(train_house_size_norm, train_price_norm):
             sess.run(optimizer, feed_dict={tf_house_size: x, tf_price: y})
 
         # Display current status
@@ -92,9 +103,9 @@ with tf.Session as sess:
     print("Trained cost=", training_cost, "size_factor=", sess.run(tf_size_factor), "price_offset=", sess.run(tf_price_offset), '\n')
 
 
-    # Plot of training and test data, and learned regression
+   # Plot of training and test data, and learned regression
 
-     # get values used to normalized data so we can denormalize data back to its original scale
+    # get values used to normalized data so we can denormalize data back to its original scale
     train_house_size_mean = train_house_size.mean()
     train_house_size_std = train_house_size.std()
 
@@ -109,16 +120,17 @@ with tf.Session as sess:
     plt.plot(train_house_size, train_price, 'go', label='Training data')
     plt.plot(test_house_size, test_house_price, 'mo', label='Testing data')
     plt.plot(train_house_size_norm * train_house_size_std + train_house_size_mean,
-          (sess.run(tf_size_factor) * train_house_size_norm + sess.run(tf_price_offset)) * train_price_std + train_price_mean,
-          label='Learned Regression')
+             (sess.run(tf_size_factor) * train_house_size_norm + sess.run(tf_price_offset)) * train_price_std + train_price_mean,
+             label='Learned Regression')
+
     plt.legend(loc='upper left')
     plt.show()
 
 
 
-     #
-     # Plot another graph that animation of how Gradient Descent sequentually adjusted size_factor and price_offset to
-     # find the values that returned the "best" fit line.
+    #
+    # Plot another graph that animation of how Gradient Descent sequentually adjusted size_factor and price_offset to
+    # find the values that returned the "best" fit line.
     fig, ax = plt.subplots()
     line, = ax.plot(house_size, house_price)
 
@@ -134,11 +146,12 @@ with tf.Session as sess:
         line.set_ydata((fit_size_factor[i] * train_house_size_norm + fit_price_offsets[i]) * train_price_std + train_price_mean)  # update the data
         return line,
 
-    # Init only required for blitting to give a clean slate.
+     # Init only required for blitting to give a clean slate.
     def initAnim():
         line.set_ydata(np.zeros(shape=house_price.shape[0])) # set y's to 0
         return line,
 
     ani = animation.FuncAnimation(fig, animate, frames=np.arange(0, fit_plot_idx), init_func=initAnim,
-                              interval=1000, blit=True)
-    plt.show()
+                                 interval=1000, blit=True)
+
+    plt.show()   
