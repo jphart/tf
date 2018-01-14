02eb921df055bf78a34b881d080de441b1240829
Jonathan Hart <jonathan@Minardi.home>
2018-01-14 18:33:19 +0000

    Update

---
+++ house_prices.py
@@ -0,0 +1,21 @@
+#Predict house prices based on house size. FT
+
+import tensorflow as tf
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+#Set up some random house sizes
+num_house = 160
+np.random.seed(42)
+house_size = np.random.randint(low=1000, high=3500, size=num_house)
+
+#Set up prices based on the size + random offset
+np.random.seed(42)
+house_price = house_size * 100.0 + np.random.randint(low=20000, high=70000, size=num_house)
+
+#Plot the house prices
+plt.plot(house_size, house_price, "bx")
+plot.ylabel("Price")
+plot.xlabel("Size")
